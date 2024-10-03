ccc2ced8844e5cc9ddfc31b25b638bd14adf608b
Subham Kumar <159040498+SubhamKumar0@users.noreply.github.com>
2024-10-03 08:48:39 +0530

    Add files via upload

---
+++ Starting Page/script.js
@@ -0,0 +1,14 @@
+// Here, you can add functionality to buttons, if needed.
+document.querySelector(".register-btn").addEventListener("click", function () {
+  alert("Register button clicked");
+});
+
+document.querySelector(".login-btn").addEventListener("click", function () {
+  alert("Login button clicked");
+});
+
+document
+  .querySelector(".get-started-btn")
+  .addEventListener("click", function () {
+    alert("Get Started button clicked");
+  });
